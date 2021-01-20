146b17d570bde11d990fbc2ba5900169dd800a81
Mathieu Lue <mathieu.lue@gmail.com>
2021-01-20 17:10:58 -0500

    updating to take schema path

--- lib/shopify_api/resources/graphql.rb
+++ lib/shopify_api/resources/graphql.rb
@@ -5,15 +5,15 @@ require 'graphql/client/http'
 module ShopifyAPI
   # GraphQL API.
   class GraphQL
-    def initialize
+    def initialize(path = '/admin/api/2020-10/graphql.json', schema = nil )
       uri = Base.site.dup
-      uri.path = '/admin/api/2020-10/graphql.json'
+      uri.path = path
       @http = ::GraphQL::Client::HTTP.new(uri.to_s) do
         define_method(:headers) do |_context|
           Base.headers
         end
       end
-      #@schema = ::GraphQL::Client.load_schema(@http)
+      @schema = schema || ::GraphQL::Client.load_schema(@http)
       @client = ::GraphQL::Client.new(schema: ::GraphQL::Schema.new, execute: @http)
     end
 
